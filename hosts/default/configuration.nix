33f0a549e4703a4517820ee1b8bb22294ab42fa5
Cypooos <60020572+Cypooos@users.noreply.github.com>
2024-07-06 14:47:28 +0200

    11 current  2024-07-06 14:47:25  24.11.20240703.9f4128e   6.6.36                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -126,6 +126,7 @@
     python3
     alejandra
     libnotify
+    neofetch
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -123,7 +123,6 @@
     gnumake
     nano
     python3
-    neofetch
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
