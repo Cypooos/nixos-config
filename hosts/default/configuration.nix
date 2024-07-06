1b456fc9e91c32afd4855a16e7ad193067d0f286
Cypooos <60020572+Cypooos@users.noreply.github.com>
2024-07-06 21:19:11 +0200

    13 current  2024-07-06 21:19:09  24.11.20240703.9f4128e   6.6.36                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -130,12 +130,12 @@
     #  wget
   ];
 
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-  };
+  #programs.steam = {
+  #  enable = true;
+  #  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  #  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  #  localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  #};
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
