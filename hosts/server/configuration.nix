88c0887ac09075780c4edcae03620a7689826bb0
Cypooos <60020572+Cypooos@users.noreply.github.com>
2024-07-06 14:42:29 +0200

    10 current  2024-07-06 14:12:56  24.11.20240703.9f4128e   6.6.36                          *

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -123,6 +123,7 @@
     gnumake
     nano
     python3
+    neofetch
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
