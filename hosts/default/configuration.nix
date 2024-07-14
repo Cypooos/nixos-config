9d163ddce4ff701df02f981d0353c1444198f598
Cypooos <60020572+Cypooos@users.noreply.github.com>
2024-07-14 19:36:36 +0200

    26 current  2024-07-14 19:36:33  24.11.20240703.9f4128e   6.6.36                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -151,7 +151,6 @@
     neofetch
     htop
     flex
-    pkg-config
 
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
