dfb6c1fed3a27f837a2810356539f1cc6b382b30
Cypooos <60020572+Cypooos@users.noreply.github.com>
2025-06-19 12:54:59 +0200

    maybe fixed hyprland

--- hosts/fermon/home.nix
+++ hosts/fermon/home.nix
@@ -50,7 +50,9 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    ".config/hypr/hyprland.conf".source = "../../modules/hyprland.conf";
+    ".config/hypr/hyprland.conf" = {
+      source = config.lib.file.mkOutOfStoreSymlink ./../../modules/hyprland.conf;
+    };
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
