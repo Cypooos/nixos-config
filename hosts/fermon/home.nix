48ec1467afa9debe497dc7f0d1c7dc483db66d59
Cypooos <60020572+Cypooos@users.noreply.github.com>
2025-06-19 15:53:09 +0200

    heuuu

--- hosts/fermon/home.nix
+++ hosts/fermon/home.nix
@@ -1,3 +1,18 @@
 { pkgs, gui, ... }:{
 
+  home = {
+    username = "cypooos"; 
+    homeDirectory = "/home/coda";
+    sessionVariables = { EDITOR = "code --wait"; };
+    stateVersion = "24.05";
+  };
+
+  programs = {
+    direnv = {
+      enable = true;
+      enableBashIntegration = true; # see note on other shells below
+      nix-direnv.enable = true;
+    };
+    home-manager.enable = true;
+  };
 }
