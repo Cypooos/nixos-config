6ff97a5a210b75cff7bcdb616fb5ea24a4356152
Cypooos <60020572+Cypooos@users.noreply.github.com>
2025-06-19 12:34:53 +0200

    maybe fixed hyprland

--- flake.nix
+++ flake.nix
@@ -44,8 +44,8 @@
             wayland.windowManager.hyprland = {
               enable = true;
               # set the flake package
-              package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-              portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+              package = inputs.hyprland.packages.${nixpkgs.stdenv.hostPlatform.system}.hyprland;
+              portalPackage = inputs.hyprland.packages.${nixpkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
             };
           }
           ./hosts/fermon/configuration.nix
