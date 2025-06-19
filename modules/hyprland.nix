d5aff6fdf8ca8739004f5d41344d0865c40108c6
Cypooos <60020572+Cypooos@users.noreply.github.com>
2025-06-19 13:06:22 +0200

    maybe fixed hyprland

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -3,22 +3,14 @@
 
     programs.hyprland = {
         enable = true;
+        withUWSM = true;
     };
+
+    # security.pam.services.hyprlock = {};
 
     environment.systemPackages = with pkgs; [
         hyprpaper
         waybar
     ];
-
-    services.greetd = {
-        enable = true;
-        settings = rec {
-        initial_session = {
-            command = "${pkgs.hyprland}/bin/hyprland";
-            user = "coda";
-        };
-        default_session = initial_session;
-        };
-    };
 
 }
