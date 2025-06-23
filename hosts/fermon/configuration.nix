5599acbf8fd22c798a4be3e0e02718ac25d5996c
Cypooos <60020572+Cypooos@users.noreply.github.com>
2025-06-23 16:47:23 +0200

    OwO

--- hosts/fermon/configuration.nix
+++ hosts/fermon/configuration.nix
@@ -165,6 +165,7 @@
     vscode
     typst
     font-awesome
+    obs-studio
     # minecraft
     # anki-bin
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
